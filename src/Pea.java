88e1c4ecbef9d13af27f71ea7a4cb893250ed62b
DellUser <DellUser@192.168.0.167>
2019-04-16 17:32:39 -0500

    Added new advance() Method to include arr of Zombies for test only

--- src/Pea.java
+++ src/Pea.java
@@ -39,6 +39,31 @@ public class Pea {
         }*/
         posX += 15;
     }
+    
+    public void advance(Zombie zomb,int numOfZomb) {
+        Rectangle pRect = new Rectangle(posX, 130 + myLane * 120, 28, 28);
+        for (int i = 0; i < numOfZomb; i++) {
+            Zombie z = zomb;
+            Rectangle zRect = new Rectangle(z.getPosX(), 109 + myLane * 120, 400, 120);
+            if (pRect.intersects(zRect)) {
+                z.setHealth(z.getHealth() - 300);
+                boolean exit = false;
+                if (z.getHealth() < 0) {
+                    System.out.println("ZOMBIE DIED");
+
+                    gp.getLaneZombies().get(myLane).remove(i);
+                    GamePanel.setProgress(10);
+                    exit = true;
+                }
+                gp.getLaneZombies().get(myLane).remove(this);
+                if (exit) break;
+            }
+        }
+        /*if(posX > 2000){
+            gp.lanePeas.get(myLane).remove(this);
+        }*/
+        posX += 15;
+    }
 
     public int getPosX() {
         return posX;
