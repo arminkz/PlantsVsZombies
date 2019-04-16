d3cff6eccf9f9806c07111855cf28411111ed401
DellUser <DellUser@192.168.0.167>
2019-04-16 17:35:17 -0500

    Added test to ensure is in test harness

--- testing/testPlantVsZombie.java
+++ testing/testPlantVsZombie.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.fail;
 import java.util.Comparator;
 import java.util.Iterator;
 
+import javax.swing.JLabel;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
@@ -16,22 +18,36 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.MethodSorters;
 import org.junit.FixMethodOrder;
+import static org.mockito.Mockito.*;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class testPlantVsZombie {
 
 	@Rule
-	public Timeout globalTimeout = new Timeout(2, SECONDS);
+	public Timeout globalTimeout = new Timeout(20, SECONDS);
 
+	
+	
+	// The first class to test is the Pea class
+	//Nick notes
+	// Will mock gameplay, because out of scope for this class
 	/*
 	 * This tests the add method - ascending and the normal iterator
 	 */
 	@Test
-	public void testInsertOne() {
+	public void testPeaAdvance() {
+		
+		
+		GamePanel gamePanelMock = new GamePanel(new JLabel ("hello"));
+		int lane = 1;
+		int xPos = 1;
+//		when(gamePanelMock.getLaneZombies().get(lane).size()).thenReturn(1);
+//		when(gamePanelMock.getLaneZombies().get(lane).get(0)).thenReturn(new Zombie(gamePanelMock,2));
+		Pea pea = new Pea(gamePanelMock,xPos,lane);
+//		Zombie zomb = new Zombie(gamePanelMock,1);
+		pea.advance();
 
-		String str = "";
-
-		assertEquals("str should return \"-1, 0, 1, 2, 2, \" ", "-1, 0, 1, 2, 2, ", str);
+		assertEquals("pea.getPosX() should return \"16 \" ", 15 , pea.getPosX());
 	}
 
 }
