c411b240fea2c5656f337f513b7eebf6cb55451a
DellUser <DellUser@192.168.0.167>
2019-04-16 19:01:03 -0500

    New Tests include normal .advance() method and the one with Zombie[]

--- testing/testPlantVsZombie.java
+++ testing/testPlantVsZombie.java
@@ -35,19 +35,36 @@ public class testPlantVsZombie {
 	 * This tests the add method - ascending and the normal iterator
 	 */
 	@Test
-	public void testPeaAdvance() {
+	public void testPeaAdvanceOne() {
 		
 		
-		GamePanel gamePanelMock = new GamePanel(new JLabel ("hello"));
+		GamePanel gamePanel = new GamePanel(new JLabel ("hello"));
 		int lane = 1;
 		int xPos = 1;
 //		when(gamePanelMock.getLaneZombies().get(lane).size()).thenReturn(1);
 //		when(gamePanelMock.getLaneZombies().get(lane).get(0)).thenReturn(new Zombie(gamePanelMock,2));
-		Pea pea = new Pea(gamePanelMock,xPos,lane);
+		Pea pea = new Pea(gamePanel,xPos,lane);
+		Zombie[] zomb = new Zombie[2];
+		zomb[0] = new Zombie(gamePanel,1);
+		zomb[1] = new Zombie(gamePanel,2);
 //		Zombie zomb = new Zombie(gamePanelMock,1);
-		pea.advance();
+		pea.advance(zomb);
 
-		assertEquals("pea.getPosX() should return \"16 \" ", 15 , pea.getPosX());
+		assertEquals("pea.getPosX() should return \"16 \" ", 16 , pea.getPosX());
 	}
 
+	@Test
+	public void testPeaAdvanceTwo() {
+		
+		
+		GamePanel gamePanel = new GamePanel(new JLabel ("hello"));
+		int lane = 1;
+		int xPos = 1;
+		Pea pea = new Pea(gamePanel,xPos,lane);
+
+		pea.advance();
+
+		assertEquals("pea.getPosX() should return \"16 \" ", 16 , pea.getPosX());
+	}
+	
 }
