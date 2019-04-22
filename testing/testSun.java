f275a793261686154a9ac713f4cc8a6ff08d1da8
gharr <gharr@DESKTOP-CQGANLU>
2019-04-22 15:00:23 -0500

    Testing commit - no actual testing done yet

---
+++ testing/testSun.java
@@ -0,0 +1,15 @@
+//George Harrington
+//testing commit
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testSun {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
