28b23caf47adb2a9b7d9f669606b51f3cd3ffba9
RINA :D <computer7214@gmail.com>
2021-05-21 02:12:45 +0900

    Refactor Collider, Pea, FreezePea class
    Rename Variable
    - Used to explain variable's role.
    - This refactoring increases the code readability.

--- src/Collider.java
+++ src/Collider.java
@@ -9,7 +9,7 @@ import java.awt.event.MouseListener;
  */
 public class Collider extends JPanel implements MouseListener {
 
-    private ActionListener al;
+    private ActionListener actionListener;
 
     public Collider() {
         //setBorder(new LineBorder(Color.RED));
@@ -21,8 +21,8 @@ public class Collider extends JPanel implements MouseListener {
 
     public Plant assignedPlant;
 
-    public void setPlant(Plant p) {
-        assignedPlant = p;
+    public void setPlant(Plant plant) {
+        assignedPlant = plant;
     }
 
     public void removePlant() {
@@ -30,12 +30,12 @@ public class Collider extends JPanel implements MouseListener {
         assignedPlant = null;
     }
 
-    public boolean isInsideCollider(int tx) {
-        return (tx > getLocation().x) && (tx < getLocation().x + 100);
+    public boolean isInsideCollider(int xPosition) {
+        return (xPosition > getLocation().x) && (xPosition < getLocation().x + 100);
     }
 
-    public void setAction(ActionListener al) {
-        this.al = al;
+    public void setAction(ActionListener actionListener) {
+        this.actionListener = actionListener;
     }
 
 
@@ -51,8 +51,8 @@ public class Collider extends JPanel implements MouseListener {
 
     @Override
     public void mouseReleased(MouseEvent e) {
-        if (al != null) {
-            al.actionPerformed(new ActionEvent(this, ActionEvent.RESERVED_ID_MAX + 1, ""));
+        if (actionListener != null) {
+            actionListener.actionPerformed(new ActionEvent(this, ActionEvent.RESERVED_ID_MAX + 1, ""));
         }
     }
 
