121a81ad8452d1133923ab3619afbc5c3f5e1655
JiHye.Song <48706964+qwer15948@users.noreply.github.com>
2022-06-08 20:53:32 +0900

    Junit test (Plant)
    	/**
    	 * Purpose: test FreezePeashooter creator
    	 * Input: FreezePeashooter(x, y)
    	 * 		x = 40, y = 10
    	 * Expected:
    	 * 		return SUCCESS
    	 * 		getX() = x = 40
    	 * 		getY() = y = 10
    	 */

--- test/Model/Plant/PlantTest.java
+++ test/Model/Plant/PlantTest.java
@@ -2,6 +2,7 @@ package Model.Plant;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.*;
+import org.junit.jupiter.api.AfterEach;
 
 import Model.Lane.Lane;
 import View.Game.GamePanel;
@@ -13,12 +14,12 @@ public class PlantTest {
 	
 	
 	@BeforeClass
-	public void setUp() throws Exception {
+	public static void setUp() throws Exception {
 		gamePanel = GamePanel.getInstance();
 		lane = Lane.getInstance();
 	}
 	
-	@AfterClass
+	@AfterEach
 	public static void tearDown() {
 		gamePanel = null;
 		lane = null;
@@ -43,4 +44,21 @@ public class PlantTest {
 		assertEquals(plant.getY(), y);
 	}
 	
+	/**
+	 * Purpose: test FreezePeashooter creator
+	 * Input: FreezePeashooter(x, y)
+	 * 		x = 40, y = 10
+	 * Expected:
+	 * 		return SUCCESS
+	 * 		getX() = x = 40
+	 * 		getY() = y = 10
+	 */
+	@Test
+	public void testCreateFreezePeashooter() {
+		int x = 40;
+		int y = 10;
+		plant = new FreezePeashooter(x, y);
+		assertEquals(plant.getX(), x);
+		assertEquals(plant.getY(), y);
+	}
 }
