cdcc0373611b9d3268905526a475d91b448da045
Yejoo-Shin <stella03s@naver.com>
2022-06-08 22:37:32 +0900

    test: function setPosition (State)

--- test/View/Element/StateTest.java
+++ test/View/Element/StateTest.java
@@ -34,48 +34,4 @@ class StateTest {
 		assertEquals(15, state.getPositionX());
 		assertEquals(25, state.getPositionY());
 	}
-
-    /**
-     * Purpose: test function getPositionX() 
-     * Input: getPositionX()
-     * Expected:
-     * return SUCCESS
-     * positionX = 10
-     */
-	@Test
-	public void testGetPositionX() {
-		State state = new State(10, 25);
-		int getX = state.getPositionX();
-		assertEquals(10, getX);
-	}
-
-    /**
-     * Purpose: test function setPositionY() 
-     * Input: setPositionY(35)
-     * Expected:
-     * return SUCCESS
-     * positionX = 15, positionY = 35
-     */
-	@Test
-	public void testSetPositionY() {
-		State state = new State(10, 25);
-		state.setPositionY(35);
-		assertEquals(10, state.getPositionX());
-		assertEquals(35, state.getPositionY());
-	}
-	
-    /**
-     * Purpose: test function getPositionY() 
-     * Input: getPositionY()
-     * Expected:
-     * return SUCCESS
-     * positionY = 25
-     */
-	@Test
-	public void testGetPositionY() {
-		State state = new State(10, 25);
-		int getY = state.getPositionY();
-		assertEquals(25, getY);
-	}
-
 }
