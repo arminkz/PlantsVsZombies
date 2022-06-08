7ee780b04d19c3435ef728720e051242ca199473
Yejoo-Shin <stella03s@naver.com>
2022-06-08 23:03:54 +0900

    test: Constructor of Sun Class

---
+++ test/View/Element/SunTest.java
@@ -0,0 +1,33 @@
+package View.Element;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import View.Game.GamePanel;
+
+class SunTest {
+    private GamePanel gamePanel;
+
+	@BeforeEach
+	void setUp() throws Exception {
+        gamePanel = GamePanel.getInstance();
+	}
+	
+    /**
+     * Purpose: test Constructor of Sun Class
+     * Input: State(10, 25, 53)
+     * Expected:
+     * return SUCCESS
+     * positionX = 10, positionY = 25, endPositionY = 53
+     */
+	@Test
+	public void testSun() {
+		Sun sun = new Sun(gamePanel, 10, 25, 53);
+		assertEquals(gamePanel, sun.getGamePanel());
+		assertEquals(10, sun.getSunState().getPositionX());
+		assertEquals(25, sun.getSunState().getPositionY());
+		assertEquals(53, sun.getSunState().getEndPositionY());
+	}
+}
