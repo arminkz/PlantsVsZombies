0fcefda49d47a445371da0609c0827efcd1bf3b9
Yejoo-Shin <stella03s@naver.com>
2022-06-08 22:53:27 +0900

    test: Constructor of SunState Class

---
+++ test/View/Element/SunStateTest.java
@@ -0,0 +1,23 @@
+package View.Element;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SunStateTest {
+
+    /**
+     * Purpose: test Constructor of SunState Class
+     * Input: State(10, 25, 53)
+     * Expected:
+     * return SUCCESS
+     * positionX = 10, positionY = 25, endPositionY = 53
+     */
+	@Test
+	public void testSunState() {
+		SunState sunState = new SunState(10, 25, 53);
+		assertEquals(10, sunState.getPositionX());
+		assertEquals(25, sunState.getPositionY());
+		assertEquals(53, sunState.getEndPositionY());
+	}
+}
